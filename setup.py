5bfee17bc11dd8ad4316aeb02149505f4d1c9ba7
Alek Slater <slayte@gmail.com>
2013-03-01 06:06:40 +0000

    added brew installation if needed for mac

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 
 import os
 import sys
+import subprocess
+
+def log(msg):
+    print msg
 
 def get_packages():
     f = open('packages.list')
@@ -12,9 +16,31 @@ def get_packages():
             packages.append(package)
     return packages
 
+def install_package_manager():
+    if sys.platform == "darwin":
+        install_brew()
+
+def has_app(app): # app = "brew"
+    try: 
+        ret = subprocess.check_output(app)
+        return True
+    except subprocess.CalledProcessError,e:
+        return True
+    except:
+        log("failed to open "+app)
+    return False
+
+def install_brew():    
+    if not has_app("brew"):
+        brew_cmd='ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"'
+        os.system(brew_cmd)
+    else:
+        log("skipping instalation of brew [Already Installed]")
+    
 # Link up bash_profile
 os.system('ln -s ~/.dotfiles/bash_profile ~/.bash_profile')
 
-# TODO: Use Brew for mac, and apt-get for ubuntu
+install_package_manager()
+
 #packages = get_packages()
 #print packages
