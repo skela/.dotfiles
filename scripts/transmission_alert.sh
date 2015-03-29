c07205170bd68b89466e75b0bbf3d119c06627df
Alek Slater <slayte@gmail.com>
2015-03-29 17:29:43 +0800

    updates to transmission scripts

--- scripts/transmission_alert.sh
+++ scripts/transmission_alert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-	
+
 #TR_APP_VERSION
 #TR_TIME_LOCALTIME
 #TR_TORRENT_DIR
@@ -7,4 +7,4 @@
 #TR_TORRENT_ID
 #TR_TORRENT_NAME
 
-python ~/.dotfiles/scripts/torrent_alert.py "$TR_TORRENT_NAME"
+python ~/.dotfiles/scripts/torrent_alert.py -n "$TR_TORRENT_NAME"
