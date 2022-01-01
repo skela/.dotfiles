22dbcb585512098769f38d803c258c8ac3ee18ee
Alek Slater <skela@davincium.com>
2022-01-01 16:07:27 +0100

    make start work on linux

--- scripts/start.py
+++ scripts/start.py
@@ -1,19 +1,31 @@
 import os
+import platform
 import json
 import pyperclip as pc
-from touchid import authenticate
+ 
+def auth() -> bool:
 
-try:
-	ok = authenticate(reason="authenticate for 1Pass")
-	if ok:
-		f = open(os.path.expanduser("~/.dotfiles/.vault"),"r")
-		s = f.read()
-		f.close()		
-		d = json.loads(s)
-		passwd = d["1PASSWORD"]
-		pc.copy(passwd)
-		print("Authentication completed")
-	else:
-		print("Failed")
-except:
-	print("Failed")
+	osname = platform.system()
+	if osname == "Darwin":
+		from touchid import authenticate
+
+		try:
+			ok = authenticate(reason="authenticate for 1Pass")
+			if ok:
+				return True
+			else:
+				return False
+		except:
+			return False
+	return True
+
+if auth():
+	f = open(os.path.expanduser("~/.dotfiles/.vault"),"r")
+	s = f.read()
+	f.close()		
+	d = json.loads(s)
+	passwd = d["1PASSWORD"]
+	pc.copy(passwd)
+	print("Authentication completed")
+else:
+	print("Auth failed")
