626ad79388d3e48eaab379f42b28826586235c50
Alek Slater <skela@davincium.com>
2022-06-21 23:58:54 +0200

    actual lua code so far

---
+++ config/nvim/lua/user/theme.lua
@@ -0,0 +1,12 @@
+local colorscheme = "gruvbox"
+
+vim.opt.termguicolors = true
+vim.opt.guifont = "monospace:h17"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	return
+end
+
+vim.cmd("highlight Normal guibg=none")
+
