025b2662b886e1fbb4ee516cbd1b2a3ebe31582b
Alek Slater <skela@davincium.com>
2022-07-10 21:43:44 +0200

    slightly less cmd space for vim

--- config/nvim/lua/user/options.lua
+++ config/nvim/lua/user/options.lua
@@ -1,7 +1,7 @@
 
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
@@ -29,7 +29,7 @@ vim.opt.relativenumber = true                   -- set relative numbered lines
 vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 8                           -- is one of my fav
+vim.opt.scrolloff = 8                           -- start scroll X before
 vim.opt.sidescrolloff = 8
 vim.opt.errorbells = false
 
