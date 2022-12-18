69fd061603f564bb82d055011bc2ac5f3cd86aaa
Alek Slater <skela@davincium.com>
2022-12-18 20:37:48 +0100

    slightly better way to lock screen (so screens dont switch on again)

--- config/qtile/lock.sh
+++ config/qtile/lock.sh
@@ -1,3 +1,3 @@
-i3lock --color 000000 & sleep 5 && xset dpms force off
-
+i3lock --color 000000 & sleep 5 && xset dpms force standby
+# i3lock --color 000000 & sleep 5 && xset dpms force off
 # lock_screen = "betterlockscreen -l dim --off 5"
