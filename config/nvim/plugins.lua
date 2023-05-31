5ebdaa8e26e99954d93e37dac69662d5385df006
Alek Slater <skela@davincium.com>
2023-05-31 08:12:21 +0200

    add some code to ensure treesitter for various languages

--- config/nvim/plugins.lua
+++ config/nvim/plugins.lua
@@ -1,5 +1,22 @@
 local plugins = {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"html",
+				"css",
+				"javascript",
+				"tsx",
+				"json",
+				"hjson",
+				"swift",
+				"dart"
+			}
+		}
+	},
+	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			require "plugins.configs.lspconfig"
