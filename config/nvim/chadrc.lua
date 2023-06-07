9cfae213a61176df556ebb73cc4fc168aeac20e0
Alek Slater <skela@davincium.com>
2023-06-07 22:21:10 +0200

    adjustments to themes, still havent gotten the background color of neovim to be black

--- config/nvim/chadrc.lua
+++ config/nvim/chadrc.lua
@@ -1,6 +1,15 @@
 ---@type ChadrcConfig 
 local M = {}
-M.ui = {theme = "gruvbox"}
+M.ui = {
+	changed_themes = {
+		gruvbox = {
+			base_16 = { base00 = "#000000" },
+			base_30 = { black = "#000000" },
+			base_46 = { black = "#000000" },
+		},
+	},
+	theme = "gruvbox",
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 return M
