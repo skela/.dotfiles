c1de7f6a8610ef35a910596e4282e7f6765445f8
Alek Slater <skela@davincium.com>
2023-06-19 23:43:20 +0200

    pressing ctrl + s in neovim saves the file, and escapes from insert mode

--- config/nvim/mappings.lua
+++ config/nvim/mappings.lua
@@ -3,7 +3,11 @@ local M = {}
 ---@param s string
 ---@return string
 local function cmd(s)
-	return "<cmd>" .. s .. "<cr>"
+	if string.find(s,"<cr>") then
+		return "<cmd>" .. s
+	else
+		return "<cmd>" .. s .. "<cr>"
+	end
 end
 
 local function next_file()
@@ -21,7 +25,7 @@ git:toggle()
 end
 
 local cmds = {}
-cmds.save_file = {cmd(":update"), "Save file"}
+cmds.save_file = {cmd(":update<cr><esc>"), "Save file"}
 cmds.close_file = {cmd(":bp<bar>sp<bar>bn<bar>bd"), "Close file"}
 cmds.open_file_tree = {cmd(":NvimTreeToggle"),"Open filetree"}
 cmds.search_file = {cmd("Telescope current_buffer_fuzzy_find"),"Search current file"}
