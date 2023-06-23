05ae026d3341d0adeff57fb7d1c7bf27f246a34f
Alek Slater <skela@davincium.com>
2023-06-23 22:32:15 +0200

    had to define some env variables for nvim and treesitter to work correctly

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,6 +6,12 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.smartindent = true
 
+if os.getenv("HOME"):find("/Users/",1,true) == 1 then
+	local sdk = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+	vim.env.C_INCLUDE_PATH = sdk .. "/usr/include/"
+	vim.env.SDKROOT = sdk
+end
+
 -- local toggleterm = require("custom.configs.toggleterm")
 -- vim.api.nvim_create_user_command("OpenTerm1", function() toggleterm.open_term1() end,{})
 -- vim.api.nvim_create_user_command("OpenTerm2", function() toggleterm.open_term2() end,{})
