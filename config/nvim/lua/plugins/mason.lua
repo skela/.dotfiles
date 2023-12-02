497aaff4a04f0e4ffbfa5231a4020acddee19bb6
Alek Slater <skela@davincium.com>
2023-12-02 22:22:45 +0100

    basic flutter nvim config using flutter-tools - disable indents via treesitter for dart as it interferes with flutter-tools

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -6,7 +6,6 @@ return {
 				"stylua",
 				"shellcheck",
 				"shfmt",
-				--"flake8",
 				"pyright",
 				"rust-analyzer",
 				"dart-debug-adapter",
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -25,7 +25,7 @@ return {
 			},
 			indent = {
 				enable = true,
-				disable = { "python" },
+				disable = { "python", "dart" },
 			},
 		},
 	},
