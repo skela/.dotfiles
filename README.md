698ba73dd9456ab1a1615e3c656c7996c390c889
Alek Slater <skela@davincium.com>
2024-01-03 23:33:30 +0100

    some more tmux goodies, started using tpm which is a tmux package manager. still havent figured out how to get ctrl+shift+f to work in nvim inside of tmux and kitty, problem is somewhere in tmux land, since it works fine in vanilla nvim + kitty

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 Config files and initial setup for fresh machines!
 Just run the setup.py file ;)
+
+# tmux
+
+You'll need to run this to get tpm:
+
+`git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm`
+
+Then once inside tmux do this:
+
+`Ctrl + a + I`
+
+
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -15,4 +15,13 @@ return {
 			},
 		},
 	},
+	{
+		"nvimtools/none-ls.nvim",
+		optional = true,
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, nls.builtins.formatting.prettier)
+		end,
+	},
 }
