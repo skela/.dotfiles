e6c33d6cb93dec375a57f9aab5f154ef27347641
Alek Slater <skela@davincium.com>
2024-01-05 16:38:34 +0100

    dap ui stuff added, but not sure how to use it yet. dap works, been able to set and step through using the Dap nvim commands

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -1,28 +1,36 @@
 return {
-	"mfussenegger/nvim-dap",
-	event = "VeryLazy",
-	config = function()
-		vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "debugBreakpoint", linehl = "", numhl = "" })
-		vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
-		vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
-		vim.fn.sign_define("DapLogPoint", { text = " ", texthl = "debugBreakpoint", linehl = "", numhl = "" })
-		vim.fn.sign_define("DapStopped", {
-			text = "",
-			texthl = "debugBreakpoint",
-			linehl = "debugPC",
-			numhl = "",
-		})
-		vim.keymap.set("n", "<F5>", function()
-			require("dap").continue()
-		end)
-		vim.keymap.set("n", "<F10>", function()
-			require("dap").step_over({})
-		end)
-		vim.keymap.set("n", "<F11>", function()
-			require("dap").step_into({})
-		end)
-		vim.keymap.set("n", "<F12>", function()
-			require("dap").step_out({})
-		end)
-	end,
+	{
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
+		config = function()
+			vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "debugBreakpoint", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "DiagnosticWarn", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "DiagnosticError", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapLogPoint", { text = " ", texthl = "debugBreakpoint", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapStopped", {
+				text = "",
+				texthl = "debugBreakpoint",
+				linehl = "debugPC",
+				numhl = "",
+			})
+			vim.keymap.set("n", "<F5>", function()
+				require("dap").continue()
+			end)
+			vim.keymap.set("n", "<F10>", function()
+				require("dap").step_over({})
+			end)
+			vim.keymap.set("n", "<F11>", function()
+				require("dap").step_into({})
+			end)
+			vim.keymap.set("n", "<F12>", function()
+				require("dap").step_out({})
+			end)
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+	},
 }
