1637f11b6f4653d6de870877f7c9945b69c68368
Alek Slater <skela@davincium.com>
2024-05-05 17:06:18 +0200

    new alacritty config - lazy update and pairs

--- config/nvim/lua/plugins/pairs.lua
+++ config/nvim/lua/plugins/pairs.lua
@@ -1,4 +1,17 @@
 return {
 	{ "echasnovski/mini.pairs", enabled = false },
-	{ "echasnovski/mini.surround" },
+	{
+		"echasnovski/mini.surround",
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surrounding in Normal and Visual modes
+				delete = "gsd", -- Delete surrounding
+				find = "gsf", -- Find surrounding (to the right)
+				find_left = "gsF", -- Find surrounding (to the left)
+				highlight = "gsh", -- Highlight surrounding
+				replace = "gsr", -- Replace surrounding so if u want to replace ' with " type gsr'"
+				update_n_lines = "gsn", -- Update `n_lines`
+			},
+		},
+	},
 }
