27e69eeaa17caa7ccd3a7f6d326627db592c9360
Alek Slater <skela@davincium.com>
2024-09-25 22:37:29 +0200

    lock screen working (for now) might have been the specific version of i3lock-color

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,11 +1,18 @@
+# betterlockscreen --off 5 -l
+
 playerctl pause
 
 sleep 0.5
 
 xset dpms force off
 
-i3lock --color 000000
+i3lock --color 000000 # i3lock-color 2.13.c.5-3 package
 
-sleep 2
-
+sleep 1
+xset dpms force off
+sleep 1
+xset dpms force off
+sleep 1
+xset dpms force off
+sleep 1
 xset dpms force off
