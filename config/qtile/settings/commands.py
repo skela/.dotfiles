1ac3120ee98b84db9ea09cf67cc51b5f004b5d4b
Alek Slater <skela@davincium.com>
2024-10-17 00:10:59 +0200

    move over thunar, think ill give nautilus a go again

--- config/qtile/settings/commands.py
+++ config/qtile/settings/commands.py
@@ -2,8 +2,7 @@ class Commands(object):
 
 	def __init__(self):
 		self.terminal = "kitty"
-		self.files = "thunar"
-		# files = "nautilus -w"
+		self.files = "nautilus -w"
 		self.discord = "/home/skela/files/apps/discord/Discord"
 		#launcher = "ulauncher --no-window-shadow"
 		#launcher = "albert toggle"
