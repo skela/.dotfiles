003eaf1933dc2eff772a13add86938c061272666
Alek Slater <skela@davincium.com>
2024-11-20 17:30:04 +0100

    tweak dashboard, need pokemon-colorscripts at home instead of pokemon-colorscripts-go... weird

--- config/nvim/lua/plugins/dashboard.lua
+++ config/nvim/lua/plugins/dashboard.lua
@@ -14,7 +14,7 @@ return {
 					{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
 					{ icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
 					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
-					{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+					{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
 					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
 				},
 
@@ -30,7 +30,7 @@ return {
 			sections = {
 				{ section = "header" },
 				{ section = "keys", gap = 1, padding = 1 },
-				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				-- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
 				-- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 				{
 					pane = 2,
@@ -46,7 +46,7 @@ return {
 				},
 				{
 					section = "terminal",
-					cmd = "pokemon-colorscripts-go --no-title; sleep .1",
+					cmd = "pokemon-colorscripts -r --no-title; sleep .1",
 					random = 10,
 					pane = 2,
 					indent = 4,
