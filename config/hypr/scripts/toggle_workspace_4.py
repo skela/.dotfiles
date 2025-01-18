30af5d2a5f02d17d99c3c694964e9e4aab0271ac
Alek Slater <skela@davincium.com>
2025-01-18 11:34:05 +0100

    have workspace switching behave like it does on qtile (in hyprland)

--- config/hypr/scripts/toggle_workspace_4.py
+++ config/hypr/scripts/toggle_workspace_4.py
@@ -8,7 +8,7 @@ if not "id" in d:
 	exit("failed to locate workspace id")
 
 if d["id"] != 4:
-	subprocess.getstatusoutput("hyprctl dispatch workspace 4")
+	# subprocess.getstatusoutput("hyprctl dispatch workspace 4")
+	subprocess.getstatusoutput("~/.dotfiles/config/hypr/scripts/qtile_like_swap.sh 4")
 else:
 	subprocess.getstatusoutput("hyprctl dispatch workspace previous")
-
