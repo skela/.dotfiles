b821f636d6bf1a2669d348a3828f0197b990e0a2
Alek Slater <skela@davincium.com>
2025-02-14 20:19:47 +0100

    cleanup setup script

--- setup.py
+++ setup.py
@@ -2,28 +2,57 @@ import argparse
 import os
 import subprocess
 import sys
+import json
+import re
+
+
+class FolderLink(object):
+
+	def __init__(self, src: str):
+		self.src = src
+
+
+class FileLink(object):
+
+	def __init__(self, src: str):
+		self.src = src
+
 
 folder_links = [
-	"config/fish",
-	"config/ghostty",
-	"config/kitty",
-	"config/alacritty",
-	"config/wezterm",
-	"config/qtile",
-	"config/hypr",
-	"config/xfce4",
-	"config/i3",
-	"config/awesome",
-	"config/tmux",
-	"config/yazi",
-	"config/flameshot",
-	"config/picom",
-	"config/variety",
-	"config/sway",
-	"config/nvim",
-	"config/vim",
-	"config/gtk-3.0",
-	"config/clipse"
+	FolderLink("config/fish"),
+	FolderLink("config/ghostty"),
+	FolderLink("config/kitty"),
+	FolderLink("config/alacritty"),
+	FolderLink("config/wezterm"),
+	FolderLink("config/qtile"),
+	FolderLink("config/hypr"),
+	FolderLink("config/hypr/swaylock"),
+	FolderLink("config/xfce4"),
+	FolderLink("config/i3"),
+	FolderLink("config/awesome"),
+	FolderLink("config/tmux"),
+	FolderLink("config/yazi"),
+	FolderLink("config/flameshot"),
+	FolderLink("config/picom"),
+	FolderLink("config/variety"),
+	FolderLink("config/sway"),
+	FolderLink("config/nvim"),
+	FolderLink("config/vim"),
+	FolderLink("config/gtk-3.0"),
+	FolderLink("config/clipse"),
+	FolderLink("config/paru"),
+	FolderLink("config/rustfmt"),
+	FolderLink("config/rofi"),
+	FolderLink("config/posting"),
+	FolderLink("config/pop-shell"),
+	FolderLink("config/polybar"),
+	FolderLink("config/yazi"),
+]
+
+file_links = [
+	FileLink("config/greenclip.toml"),
+	FileLink("config/electron-flags.conf"),
+	FileLink("config/albert.conf"),
 ]
 
 
@@ -35,29 +64,33 @@ def get_input(msg):
 	return input(msg)
 
 
-package_managers = ["apt-get", "yum"]
-
-# Packages
+package_managers = ["apt-get", "apt", "yum", "yay", "paru"]
 
 
-def get_packages(server_packages):
-	s = 'packages.list'
-	if server_packages:
-		s = 'packages_server.list'
+def get_packages(server_packages: bool):
+	s = "packages.jsonc"
 	f = open(s)
 	packs_raw = f.read()
 	f.close()
+
+	packs_raw = re.sub(r'//.*?$', '', packs_raw, flags=re.MULTILINE)
+	packs_raw = re.sub(r'/\*.*?\*/', '', packs_raw, flags=re.DOTALL)
+
 	packages = []
-	for package in packs_raw.split('\n'):
-		if not package.startswith('#') and len(package) > 0:
-			packages.append(package)
+	ar = json.loads(packs_raw)
+	for package in ar:
+		if server_packages:
+			if "tags" in package and "server" in package["tags"]:
+				packages.append(package["name"])
+		else:
+			packages.append(package["name"])
 	return packages
 
 
 def install_package_manager():
 	p = sys.platform
 	if p == "darwin":
-		install_ports()
+		install_brew()
 	if p == "linux2" or p == "linux":
 		check_package_managers()
 	if p == "win32" or p == "win64":
@@ -69,12 +102,12 @@ def install_packages(server_packages=False):
 	packages = get_packages(server_packages)
 	log(str(packages))
 	r = get_input("Would you like to install these packages? (y/n) ")
-	if r == 'y':
-		log('TODO: Fill in this function (install_packages)')
-	elif r == 'n':
-		log('Ok, as you wish')
+	if r == "y" or r == "Y":
+		log("TODO: Fill in this function (install_packages)")
+	elif r == "n":
+		log("Ok, as you wish")
 	else:
-		log('Err... ok ? (Unknown input)')
+		log("Err... ok ? (Unknown input)")
 
 
 # Utils
@@ -84,22 +117,31 @@ def linkup(filename, destfilename=None):
 	dstname = filename
 	if destfilename is not None:
 		dstname = destfilename
-	src = '~/.dotfiles/' + filename
-	dst = '~/.' + dstname
-	cmd = 'ln -s ' + src + ' ' + dst
+	src = "~/.dotfiles/" + filename
+	dst = "~/." + dstname
+	cmd = "ln -s " + src + " " + dst
 	os.system(cmd)
 
 
-def linkup_folder(folder):
-	src = os.path.join(os.path.expanduser("~/.dotfiles/"), folder)
+def linkup_folder(folder: FolderLink):
+	src = os.path.join(os.path.expanduser("~/.dotfiles/"), folder.src)
 	dst = os.path.join(os.path.expanduser("~/"), ".config")
+
+	cmd = f"ln -s {src} {dst}"
+	os.system(cmd)
+
+
+def linkup_file(file: FileLink):
+	src = os.path.join(os.path.expanduser("~/.dotfiles/"), file.src)
+	dst = os.path.join(os.path.expanduser("~/"), ".config")
+
 	cmd = f"ln -s {src} {dst}"
 	os.system(cmd)
 
 
 def mkdir_if_needed(folder: str):
-	dst = '~/.' + folder
-	cmd = f'mkdir -p {dst}'
+	dst = "~/." + folder
+	cmd = f"mkdir -p {dst}"
 	os.system(cmd)
 
 
@@ -112,8 +154,8 @@ def has_app(app, arg=None):    # app = "brew"
 		return True
 	except subprocess.CalledProcessError:
 		return True
-	except:
-		log("failed to open " + app)
+	except Exception as _:
+		pass
 	return False
 
 
@@ -125,16 +167,6 @@ def install_brew():
 		log("skipping instalation of brew [Already Installed]")
 
 
-def install_ports():
-	if not has_app("port", 'version'):
-		port_cmd = "open http://www.macports.org/install.php"
-		log("Please install ports manually.")
-		os.system(port_cmd)
-		get_input("Input anything to continue. ")
-	else:
-		log("skipping instalation of ports [Already Installed]")
-
-
 def install_oh_my_zsh():
 	if not os.path.exists(os.path.expanduser("~/.oh-my-zsh")):
 		cmd = 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
@@ -142,6 +174,7 @@ def install_oh_my_zsh():
 
 
 def check_package_managers():
+	log("Checking package managers...")
 	for pm in package_managers:
 		if has_app(pm):
 			log("This system has " + pm + " installed.")
@@ -170,10 +203,10 @@ if args.linkup:
 	linkup("bash_profile")
 	linkup("bash_profile", "bashrc")
 	linkup("gitconfig")
-	linkup("screenrc")
-	linkup("tmux.conf")
 	linkup("private/ssh.config", "ssh/config")
-	linkup("zshrc")
 
 	for link in folder_links:
 		linkup_folder(link)
+
+	for link in file_links:
+		linkup_file(link)
