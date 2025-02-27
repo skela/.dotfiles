1d18e39333e355c834331b5a2e3b0cf5798cc3fc
Alek Slater <skela@davincium.com>
2025-02-27 17:02:40 +0100

    enables debugging of unit tests, but need to see how i can avoid neotest overwriting all the quicktest keybinds

--- config/nvim/lua/plugins/tests.lua
+++ config/nvim/lua/plugins/tests.lua
@@ -40,102 +40,136 @@
 -- }
 --
 return {
-	-- "skela/quicktest.nvim",
-	-- branch = "feature/dart",
-	"quolpr/quicktest.nvim",
-	-- dir = "~/code/quicktest.nvim/",
-	-- dev = { true },
-	config = function()
-		local qt = require("quicktest")
 
-		qt.setup({
-			adapters = {
-				-- require("quicktest.adapters.dart"),
-				-- require("quicktest.adapters.golang")({
-				-- 	additional_args = function(bufnr) return { "-race", "-count=1" } end,
-				-- 	-- bin = function(bufnr) return 'go' end
-				-- 	-- cwd = function(bufnr) return 'your-cwd' end
-				-- }),
-				-- require("quicktest.adapters.vitest"),
-				-- require("quicktest.adapters.elixir"),
-				-- require("quicktest.adapters.criterion"),
-				require("quicktest.adapters.golang"),
-				require("quicktest.adapters.dart"),
-			},
-			default_win_mode = "popup",
-		})
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"m00qek/baleia.nvim",
+	{
+		"sidlatau/neotest-dart",
+		dependencies = {
+			"nvim-neotest/neotest",
+		},
+		lazy = false,
 	},
-	keys = {
-		{
-			"<leader>tr",
-			function()
-				local qt = require("quicktest")
-				-- current_win_mode return currently opened panel, split or popup
-				qt.run_line()
-				-- You can force open split or popup like this:
-				-- qt().run_current('split')
-				-- qt().run_current('popup')
-			end,
-			desc = "[T]est [R]un",
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"sidlatau/neotest-dart",
 		},
-		{
-			"<leader>tR",
-			function()
-				local qt = require("quicktest")
-
-				qt.run_file()
-			end,
-			desc = "[T]est [R]un file",
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-dart")({
+						command = "flutter",
+						use_lsp = true,
+					}),
+				},
+			})
+		end,
+		keys = {
+			{ "<leader>tD", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug Nearest" },
 		},
-		{
-			"<leader>td",
-			function()
-				local qt = require("quicktest")
+	},
 
-				qt.run_dir()
-			end,
-			desc = "[T]est Run [D]ir",
+	{
+		-- "skela/quicktest.nvim",
+		-- branch = "feature/dart",
+		"quolpr/quicktest.nvim",
+		-- dir = "~/code/quicktest.nvim/",
+		-- dev = { true },
+		config = function()
+			local qt = require("quicktest")
+
+			qt.setup({
+				adapters = {
+					-- require("quicktest.adapters.dart"),
+					-- require("quicktest.adapters.golang")({
+					-- 	additional_args = function(bufnr) return { "-race", "-count=1" } end,
+					-- 	-- bin = function(bufnr) return 'go' end
+					-- 	-- cwd = function(bufnr) return 'your-cwd' end
+					-- }),
+					-- require("quicktest.adapters.vitest"),
+					-- require("quicktest.adapters.elixir"),
+					-- require("quicktest.adapters.criterion"),
+					require("quicktest.adapters.golang"),
+					require("quicktest.adapters.dart"),
+				},
+				default_win_mode = "popup",
+			})
+		end,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"m00qek/baleia.nvim",
 		},
-		{
-			"<leader>ta",
-			function()
-				local qt = require("quicktest")
+		keys = {
+			{
+				"<leader>tr",
+				function()
+					local qt = require("quicktest")
+					-- current_win_mode return currently opened panel, split or popup
+					qt.run_line()
+					-- You can force open split or popup like this:
+					-- qt().run_current('split')
+					-- qt().run_current('popup')
+				end,
+				desc = "[T]est [R]un",
+			},
+			{
+				"<leader>tR",
+				function()
+					local qt = require("quicktest")
 
-				qt.run_all()
-			end,
-			desc = "[T]est Run [A]ll",
-		},
-		{
-			"<leader>tp",
-			function()
-				local qt = require("quicktest")
+					qt.run_file()
+				end,
+				desc = "[T]est [R]un file",
+			},
+			{
+				"<leader>td",
+				function()
+					local qt = require("quicktest")
 
-				qt.run_previous()
-			end,
-			desc = "[T]est Run [P]revious",
-		},
-		{
-			"<leader>tt",
-			function()
-				local qt = require("quicktest")
+					qt.run_dir()
+				end,
+				desc = "[T]est Run [D]ir",
+			},
+			{
+				"<leader>ta",
+				function()
+					local qt = require("quicktest")
 
-				qt.toggle_win("popup")
-			end,
-			desc = "[T]est [T]oggle popup window",
-		},
-		{
-			"<leader>ts",
-			function()
-				local qt = require("quicktest")
+					qt.run_all()
+				end,
+				desc = "[T]est Run [A]ll",
+			},
+			{
+				"<leader>tp",
+				function()
+					local qt = require("quicktest")
 
-				qt.toggle_win("split")
-			end,
-			desc = "[T]est Toggle [S]plit window",
+					qt.run_previous()
+				end,
+				desc = "[T]est Run [P]revious",
+			},
+			{
+				"<leader>tt",
+				function()
+					local qt = require("quicktest")
+
+					qt.toggle_win("popup")
+				end,
+				desc = "[T]est [T]oggle popup window",
+			},
+			{
+				"<leader>ts",
+				function()
+					local qt = require("quicktest")
+
+					qt.toggle_win("split")
+				end,
+				desc = "[T]est Toggle [S]plit window",
+			},
 		},
 	},
 }
