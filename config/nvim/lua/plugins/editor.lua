54426c89bcb6946f47fe0973f7106e0812c34c99
Alek Slater <skela@davincium.com>
2025-05-01 15:49:14 +0200

    update nvim packages - add markdown preview (excellent md previewer in the browser)

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -170,4 +170,11 @@ return {
 			},
 		},
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+		init = function() vim.g.mkdp_browser = "firefox-developer-edition" end,
+	},
 }
