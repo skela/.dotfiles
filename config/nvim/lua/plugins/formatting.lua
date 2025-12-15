9a0b56d984a06f8cad973c9f327ff7e2ac16c6b7
Alek Slater <skela@davincium.com>
2025-12-15 09:41:43 +0100

    swift adjustments for the fold/unfold function

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -67,8 +67,55 @@ return {
 		keys = { "<space>m" },
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
+			local lang_utils = require("treesj.langs.utils")
+
 			require("treesj").setup({
 				max_join_length = 500,
+				langs = {
+					swift = {
+						collection_literal = lang_utils.set_default_preset({
+							both = {
+								separator = ",",
+							},
+						}),
+						value_arguments = lang_utils.set_default_preset({
+							both = {
+								separator = ",",
+							},
+						}),
+						statements = lang_utils.set_preset_for_non_bracket({
+							split = {
+								recursive_ignore = {
+									"value_arguments",
+									"function_value_parameters",
+								},
+							},
+							join = {
+								force_insert = ";",
+								no_insert_if = {
+									lang_utils.helpers.if_penultimate,
+								},
+							},
+						}),
+						lambda_literal = {
+							target_nodes = {
+								"statements",
+							},
+						},
+						function_body = {
+							target_nodes = {
+								"statements",
+							},
+						},
+						property_declaration = {
+							target_nodes = {
+								"collection_literal",
+								"value_arguments",
+							},
+						},
+						function_value_parameters = lang_utils.set_preset_for_args(),
+					},
+				},
 			})
 		end,
 	},
