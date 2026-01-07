417c1c85c201469ec5c7de22844f9ec4ee6f3a43
Alek Slater <skela@davincium.com>
2026-01-07 23:15:39 +0100

    actually make use of tmux navigator

--- config/nvim/lua/plugins/tmux.lua
+++ config/nvim/lua/plugins/tmux.lua
@@ -2,5 +2,24 @@ return {
 	{
 		"christoomey/vim-tmux-navigator",
 		lazy = false,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			-- "TmuxNavigatePrevious",
+			-- "TmuxNavigatorProcessList",
+		},
+		keys = {
+			-- { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			-- { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			-- { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			-- { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+			{ "<c-Left>", "<cmd>TmuxNavigateLeft<cr>" },
+			{ "<c-Down>", "<cmd>TmuxNavigateDown<cr>" },
+			{ "<c-Up>", "<cmd>TmuxNavigateUp<cr>" },
+			{ "<c-Right>", "<cmd>TmuxNavigateRight<cr>" },
+		},
 	},
 }
