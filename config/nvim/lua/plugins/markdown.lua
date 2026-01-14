f1f54ddd8a4f6c3633ef19668ad56f50a1336ff2
Alek Slater <skela@davincium.com>
2026-01-14 17:25:41 +0100

    meh

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,78 @@
+-- local function find_all(buf, node, pattern)
+-- 	local start_row, _, end_row, _ = node:range()
+-- 	end_row = end_row + (start_row == end_row and 1 or 0)
+-- 	local lines = vim.api.nvim_buf_get_lines(buf, start_row, end_row, false)
+-- 	local result = {}
+-- 	for row, line in ipairs(lines) do
+-- 		---@type integer|nil
+-- 		local index = 1
+-- 		while index ~= nil do
+-- 			local start_index, end_index = line:find(pattern, index)
+-- 			if start_index == nil or end_index == nil then
+-- 				index = nil
+-- 			else
+-- 				table.insert(result, {
+-- 					row = start_row + row - 1,
+-- 					col = { start_index - 1, end_index },
+-- 				})
+-- 				index = end_index + 1
+-- 			end
+-- 		end
+-- 	end
+-- 	return result
+-- end
+-- local function mark(match, text)
+-- 	return {
+-- 		conceal = true,
+-- 		start_row = match.row,
+-- 		start_col = match.col[1],
+-- 		opts = {
+-- 			end_col = match.col[2],
+-- 			conceal = "",
+-- 			virt_text = { { text, "DiagnosticOk" } },
+-- 			virt_text_pos = "inline",
+-- 		},
+-- 	}
+-- end
+-- local function render_dashes(root, buf)
+-- 	local query = vim.treesitter.query.parse("markdown_inline", '((inline) @replacements (#lua-match? @replacements "[%.%-][%.%-]"))')
+-- 	local result = {}
+-- 	for _, node in query:iter_captures(root, buf) do
+-- 		local _, start_col, _, _ = node:range()
+-- 		if start_col == 0 then
+-- 			-- 3 or more dots
+-- 			local ellipses = find_all(buf, node, "%.%.%.+")
+-- 			for _, ellipse in ipairs(ellipses) do
+-- 				table.insert(result, mark(ellipse, "…"))
+-- 			end
+-- 			-- 2 or more hyphens
+-- 			local dashes = find_all(buf, node, "%-%-+")
+-- 			for _, dash in ipairs(dashes) do
+-- 				local width = dash.col[2] - dash.col[1]
+--
+-- 				local ems = math.floor(width / 3)
+-- 				width = math.fmod(width, 3)
+--
+-- 				local ens = math.floor(width / 2)
+-- 				width = math.fmod(width, 2)
+--
+-- 				local text = string.rep("—", ems) .. string.rep("–", ens) .. string.rep("-", width)
+--
+-- 				table.insert(result, mark(dash, text))
+-- 			end
+-- 		end
+-- 	end
+-- 	return result
+-- end
+
 return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		opts = {
 			file_types = { "markdown", "Avante" },
+			-- custom_handlers = {
+			-- 	markdown_inline = { extends = true, parse = render_dashes },
+			-- },
 			bullet = {
 				icons = { "•", "◦", "▪" },
 				right_pad = 1,
