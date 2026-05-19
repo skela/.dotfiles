8a455ced64f8f165df6dfcfd62d35c23b8ec1d1b
Alek Slater <skela@davincium.com>
2026-05-20 00:27:06 +0200

    fixes hypridle / lock issues
    also had to downgrade to an older version of sdbus-cpp since hypridle
    crashes with the current most recent version
    
    sudo pacman -U
    /var/cache/pacman/pkg/sdbus-cpp-2.2.1-1-x86_64.pkg.tar.zst

--- config/hypr/scripts/check_lock_and_dpms.sh
+++ config/hypr/scripts/check_lock_and_dpms.sh
@@ -2,5 +2,5 @@
 
 # Check if hyprlock is running (indicating the session is locked)
 if pidof hyprlock >/dev/null; then
-	hyprctl dispatch 'hl.dsp.dpms({action="off"})'
+	hyprctl dispatch 'hl.dsp.dpms({action="disable"})'
 fi
